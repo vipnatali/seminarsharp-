b69a5e23b293db533b376710812ed547264428fb
Natali <vipnatalii@bk.ru>
2022-11-17 12:06:37 +0500

    Решена 16 задача

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -10,27 +10,30 @@
 
 // 32679 -> 6
 
-// Console.WriteLine("Введите любое число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string numfirst = Convert.ToString(num);
+int numDigit = numfirst.Length;
+
+if (numDigit > 2)
+{
+    Console.WriteLine($"Третья цифра {numfirst[2]} ");
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+
+}
 
 
-// if ( num <= 2)
-// {
-//     Console.WriteLine("Третьей цифры нет");
-// }
-// else
-// {
-    
-// if (num = num % 10);
-//     Console.WriteLine($"Третья цифра: {num}");
-// }
 
-      Console.Write("Введите любое число: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            string numStr = Convert.ToString(num);
-             int numDigit = numStr.Length;
-            if (numDigit > 2) Console.WriteLine($"Третья цифра введённого числа: {numStr[2]}");
-            else Console.WriteLine("третьей цифры нет");
-      
+
+
+
+
+
+
+
+
 
 
---
+++ Task16/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//  Напишите программу, 
+//  которая принимает на вход цифру, обозначающую день недели, 
+//  и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру: ");
+string num = Console.ReadLine();
+
+if(num == "1")
+Console.WriteLine("нет");
+else if(num == "2")
+Console.WriteLine("нет");
+else if(num == "3")
+Console.WriteLine("нет");
+else if(num == "4")
+Console.WriteLine("нет");
+else if(num == "5")
+Console.WriteLine(" нет");
+else if(num == "6")
+Console.WriteLine("да");
+else if (num == "7")
+Console.WriteLine("да");
+
+else Console.WriteLine("Увы, такого дня недели нет!");
+
