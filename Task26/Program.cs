0d6e1dd13b7b974f8611f2ea4785889bef45f83a
Natali <vipnatalii@bk.ru>
2022-11-22 14:33:07 +0500

    Решены задачи 25, 26, 27

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int SumDigits(int num)
+{
+    int sum = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
+
+int sumDigits = SumDigits(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumDigits}");
